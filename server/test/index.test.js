15846bba7d5e6418ff584dd7f434d448f0515a95
girlkig <taprekuma@yahoo.co.uk>
2019-06-21 23:14:52 +0100

    chore(setup): setup development environment
    Setup devlopment and test environment
    
    [Delivers: #16685395]
    
     On branch ch-setup-delopmet-evironment-166853695
     Changes to be committed:
    	new file:   .babelrc
    	new file:   .editorconfig
    	new file:   .env.example
    	new file:   .eslintrc
    	new file:   .gitignore
    	new file:   .travis.yml
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   server/app.js

---
+++ server/test/index.test.js
@@ -0,0 +1,30 @@
+import chai from 'chai';
+import chaiHTTP from 'chai-http';
+import app from '../app';
+
+const { expect } = chai;
+chai.use(chaiHTTP);
+
+describe('GET /', () => {
+  it('should return status 200 and a message', (done) => {
+    chai
+      .request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
+
+describe('No matching routes', () => {
+  it('should return status 404 and a message', (done) => {
+    chai
+      .request(app)
+      .get('/dsddsdsds')
+      .end((err, res) => {
+        expect(res.status).to.be.equal(404);
+        done();
+      });
+  });
+});
