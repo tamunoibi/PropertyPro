630f0aeded9f678c3fc4f7193e55cd0ada45ab9f
girlkig <taprekuma@yahoo.co.uk>
2019-07-16 12:38:39 +0100

    test

--- src/v1/routes/propertyRoute.js
+++ src/v1/routes/propertyRoute.js
@@ -20,26 +20,14 @@ const {
 
 
 // These routes are only available to agents
-propertyRouter.post('/', createProperty);
-propertyRouter.patch('/:propertyId', isAdmin, updateProperty);
-propertyRouter.patch('/:propertyId/sold', isAdmin, markAsSold);
-propertyRouter.delete('/:propertyId', isAdmin, deleteProperty);
+propertyRouter.post('/', isAdmin, validateCreateProperty, createProperty);
+propertyRouter.patch('/:propertyId', validateParam, isAdmin, validateUpdateProperty, updateProperty);
+propertyRouter.patch('/:propertyId/sold', validateParam, isAdmin, markAsSold);
+propertyRouter.delete('/:propertyId', validateParam, isAdmin, deleteProperty);
 
 
 // These routes are only available to a logged in users(that is both an agent and a user)
 propertyRouter.get('/:propertyId', getSpecificProperty);
 propertyRouter.get('/', getAllProperty);
-
-
-// // These routes are only available to agents
-// propertyRouter.post('/', isAdmin, validateCreateProperty, createProperty);
-// propertyRouter.patch('/:propertyId', validateParam, isAdmin, validateUpdateProperty, updateProperty);
-// propertyRouter.patch('/:propertyId/sold', validateParam, isAdmin, markAsSold);
-// propertyRouter.delete('/:propertyId', validateParam, isAdmin, deleteProperty);
-
-
-// // These routes are only available to a logged in users(that is both an agent and a user)
-// propertyRouter.get('/:propertyId', getSpecificProperty);
-// propertyRouter.get('/', getAllProperty);
 
 export default propertyRouter;
